a8130c5c8d1322355d19dbe650a5121606d9eb71
Dakshesh <pdakshesh424@gmail.com>
2024-06-19 12:05:00 +0530

    'chnages'

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const cors=require('cors')
 app.use(cors());
 
 app.get('/',(req,res)=>{
-    res.json({message:'working fine'})
+    res.status(200).send('Landing page')
 })
-
+app.get('/page',(req,res)=>{
+res.json({message:'good working'})
+})
 app.listen(4000,()=>{
     console.log('object')
 })
